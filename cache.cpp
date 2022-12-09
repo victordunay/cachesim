c6c5d96c8a557a4e1e00f55c2e22edd5335249c5
ran <ran-k@campus.technion.ac.il>
2022-12-09 19:04:16 +0200

    control bits and int LRU

--- cache.cpp
+++ cache.cpp
@@ -58,7 +58,7 @@ typedef struct
 //    there is allways valid, and allways LRU. NO dirty bit when write-through 
     bool valid;
     bool dirty;
-    bool LRU;
+    int LRU;
 
     status_t status;
 } set_t;
