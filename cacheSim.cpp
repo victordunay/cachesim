088e2c487246938d8f510e8492fd09a3f9db79e8
ran <ran-k@campus.technion.ac.il>
2022-12-11 20:33:18 +0200

    avgAccTime-fix

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -159,9 +159,7 @@ int main(int argc, char **argv)
 	double L2MissRate= (cache.l2->num_of_miss)/(1.0*cache.l2->num_of_access);
 
 	double tot_mem_access= cache.l1->num_of_access +cache.l2->num_of_access + cache.l2->num_of_miss; 
-
-	//double avgAccTime= ((cache.l1->num_of_access* (L1Cyc) )  +  (L1MissRate)*(L2Cyc)  +  (L2MissRate)*(MemCyc)) / tot_mem_access ;
-	double avgAccTime= (( (L1Hits)* (L1Cyc) )  +  (L1MissRate)*(L2Hits)*(L1Cyc+L2Cyc)  +  (L1MissRate)*(L2MissRate)*(L1Cyc+L2Cyc+MemCyc))/* / tot_mem_access */;																			
+	double avgAccTime= (( (L1Hits)* (L1Cyc) )  +  (L1MissRate)*(L2Hits)*(L1Cyc+L2Cyc)  +  (L1MissRate)*(L2MissRate)*(L1Cyc+L2Cyc+MemCyc));																			
 	printf("L1miss=%.03f ", L1MissRate);												
 	printf("L2miss=%.03f ", L2MissRate);
 	printf("AccTimeAvg=%.03f\n", avgAccTime);
